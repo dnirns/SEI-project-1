a3203adf90ee137aa74164874edbbda39ea68285
dnirns <daniel.irons@gmail.com>
2020-07-22 13:41:23 +0100

    charlotte help 🐛 fix - movement / collosion working

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,7 @@
 function init() {
   //* GENERATE GRID //
-
   const width = 16
   const numberOfCells = width * width
-
   function makeGrid() {
     for (let i = 0; i < numberOfCells; i++) {
       const cell = document.createElement('div')
@@ -12,81 +10,51 @@ function init() {
       grid.appendChild(cell)
     }
   }
-
   //* GAME ARRAYS AND GLOBAL VARIABLES //
-  let cells = []
-  let enemiesRow1Array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const cells = []
+  const enemiesRow1Array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   const enemiesRow2Array = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
   const enemiesRow3Array = [32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
   const enemiesRow4Array = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
-  let enemiesRow5Array = [64, 65, 66, 67, 68, 69, 70, 71, 72, 73]
-  // let enemyPosition = 0
+  const enemiesRow5Array = [64, 65, 66, 67, 68, 69, 70, 71, 72, 73]
+  let enemyPositions = enemiesRow1Array.concat(enemiesRow2Array).concat(enemiesRow3Array).concat(enemiesRow4Array).concat(enemiesRow5Array)
+  let enemyDirection = true
+  let movesSinceDirectionSwitch = 0
+  let maxMovesInDirection = 6
   let playerPosition = 240
   let laserPosition = playerPosition
-
   //* DOM ELEMENTS //
   const grid = document.querySelector('.game-grid')
   const startButton = document.querySelector('#start-button')
   const resetButton = document.querySelector('#reset-button')
-
-
   //* DOM EXECUTION //
-
   //? CREATE PLAYER AND ENEMIES
-
   function createPlayer() {
     cells[playerPosition].classList.add('player')
   }
-
-  function createEnemyRow1() {
-    enemiesRow1Array.forEach(enemy => cells[enemy].classList.add('enemy'))
-  }
-
   // function createEnemyRow2() {
   //   enemiesRow2Array.forEach(enemy => cells[enemyPosition + enemy].classList.add('enemy-row-2' , 'enemy'))
   // }
-
   // function createEnemyRow3() {
   //   enemiesRow3Array.forEach(enemy => cells[enemyPosition + enemy].classList.add('enemy-row-3' , 'enemy'))
   // }
-
   // function createEnemyRow4() {
   //   enemiesRow4Array.forEach(enemy => cells[enemyPosition + enemy].classList.add('enemy-row-4' , 'enemy'))
   // }
-
   // function createEnemyRow5() {
   //   enemiesRow5Array.forEach(enemy => cells[enemyPosition + enemy].classList.add('enemy-row-5' , 'enemy'))
   // }
-  function createLaser() {
-    cells[laserPosition].classList.add('laser')
-  }
-
-  function removeLaser() {
-    cells[laserPosition].classList.remove('laser')
-  }
-
   //? REMOVE PLAYER AND ENEMIES
-
   function removePlayer() {
     cells[playerPosition].classList.remove('player')
   }
-
   function removeAllEnemies() {
-    enemiesRow1Array.forEach(enemy => cells[enemy].classList.remove('enemy'))
-    enemiesRow2Array.forEach(enemy => cells[enemy].classList.remove('enemy'))
-    enemiesRow3Array.forEach(enemy => cells[enemy].classList.remove('enemy'))
-    enemiesRow4Array.forEach(enemy => cells[enemy].classList.remove('enemy'))
-    enemiesRow5Array.forEach(enemy => cells[enemy].classList.remove('enemy'))
+    enemyPositions.forEach(enemy => cells[enemy].classList.remove('enemy'))
   }
   // CREATE ALL ENEMY ROWS COMBINED FUNCTION
   function createAllEnemies() {
-    createEnemyRow1()
-    // createEnemyRow2()
-    // createEnemyRow3()
-    // createEnemyRow4()
-    // createEnemyRow5()
+    enemyPositions.forEach(enemy => cells[enemy].classList.add('enemy'))
   }
-
   //* DOM EVENTS //
   //CLICKS
   resetButton.addEventListener('click', resetClick)
@@ -95,56 +63,37 @@ function init() {
   document.addEventListener('keyup', startEnter)
   document.addEventListener('keydown', movePlayer)
   document.addEventListener('keyup', resetKey)
-
-
-
-
-
-
-
-
-
   //* GAME FUNCTIONS //
-
   //? START GAME - BUTTON AND ENTER KEY
-
   function startClick() {
-    createPlayer()
-    createAllEnemies()
-    moveEnemy()
+    // createPlayer()
+    // createAllEnemies()
+    moveEnemies()
   }
-
   function startEnter(e) {
     if (e.keyCode === 13) {
-      createPlayer()
-      createAllEnemies()
-      moveEnemy()
+      // createPlayer()
+      // createAllEnemies()
+      moveEnemies()
     }
   }
-
   //? RESET GAME - BUTTON AND KEYBOAD INPUT
   function resetClick() {
     clearInterval(enemyTimerId)
     removePlayer()
     playerPosition = 240
     removeAllEnemies()
-    enemyPosition = 0
   }
-
   function resetKey(e) {
     if (e.keyCode === 82) {
       clearInterval(enemyTimerId)
       removePlayer()
       playerPosition = 240
       removeAllEnemies()
-      enemyPosition = 0
     }
   }
-
   //* MOVEMENT //
-
   //? MOVE PLAYER WITH KEYS
-
   function movePlayer(e) {
     const playerMoveLeft = playerPosition - 1
     const playerMoveRight = playerPosition + 1
@@ -160,116 +109,90 @@ function init() {
       moveLaser()
     }
   }
-
-
   //* ENEMY MOVEMENT
 
-  const enemyDirectionsPattern = [
-    1, 1, 1, 1, 1, 1,
-    width,
-    -1, -1, -1, -1,
-    width,
-    1, 1
-  ]
 
-  let enemiesActions = null
-
-  function moveEnemy() {
-    enemiesActions = setInterval(() => {
-      enemiesRow1Array.forEach(enemy => cells[enemy].classList.remove('enemy'))
-
-      enemiesRow1Array = enemiesRow1Array.map(enemy => {
-        return enemy + enemyDirectionsPattern
-      })
-
-      enemiesRow1Array.forEach(enemy => {
-        cells[enemy].classList.add('enemy')
-      })
-
-
-      // cells[enemy].classList.add('enemy')  
+  function removeAllEnemyClasses() {
+    enemyPositions.forEach(enemy => cells[enemy].classList.remove('enemy'))
+  }
+  function moveEnemiesRight() {
+    removeAllEnemyClasses()
+    enemyPositions = enemyPositions.map(enemy => enemy + 1)
+    createAllEnemies()
+  }
+  function moveEnemiesLeft() {
+    removeAllEnemyClasses()
+    enemyPositions = enemyPositions.map(enemy => enemy - 1)
+    createAllEnemies()
+  }
+  function moveEnemiesDown() {
+    removeAllEnemyClasses()
+    enemyPositions = enemyPositions.map(enemy => enemy += width)
+    createAllEnemies()
+  }
+  let enemyTimerId = null
+  function moveEnemies() {
+    console.log('calling move enemy')
+    clearInterval(enemyTimerId)
+    enemyTimerId = setInterval(() => {
+      console.log('starting enemy timer')
+      if (enemyDirection) {
+        moveEnemiesRight()
+      } else {
+        moveEnemiesLeft()
+      }
+      movesSinceDirectionSwitch++
+      if (movesSinceDirectionSwitch >= maxMovesInDirection) {
+        movesSinceDirectionSwitch = 0
+        enemyDirection = !enemyDirection
+        moveEnemiesDown()
+      }
+      // console.log('enemies have moved: ', enemyPositions)
     }, 500)
   }
 
-  // function enemyGridPosition() {
-  //   enemiesRow1Array.forEach(enemy => cells[enemy].classList.add('enemy'))
-  // }
-  // enemyGridPosition()
-  // // function moveRight() {
-  //   removeAllEnemies()
-  //   // enemyPosition = enemyPosition + 1
-  //   createAllEnemies()
-  // }
-
-  // function moveLeft() {
-  //   removeAllEnemies()
-  //   // enemyPosition = enemyPosition - 1
-  //   createAllEnemies()
-  // }
-
-  // let enemyTimerId = null
-
-  // function moveEnemy() {
-  //   let isMovingRight = true
-  //   let numberOfMoves = 0
-  //   enemyTimerId = setInterval(() => {
-  //     if (isMovingRight) {
-  //       moveRight()
-  //     } else {
-  //       moveLeft()
-  //     }
-  //     numberOfMoves++
-  //     if (numberOfMoves === 6) {
-  //       createEnemyRow1()
-  //       numberOfMoves = 0
-  //       isMovingRight = !isMovingRight
-  //     }
-  //   }, 500)
-  // }
-
-
+  
   //* PLAYER SHOOT
-
-
+  function createLaser() {
+    cells[laserPosition].classList.add('laser')
+  }
+  function removeLaser() {
+    cells[laserPosition].classList.remove('laser')
+  }
   let laserTimerId = null
-
   function moveLaser() {
     laserPosition = playerPosition
-
+    const laserMovingUp = true
+    clearInterval(laserTimerId)
     laserTimerId = setInterval(() => {
-      if (cells[laserPosition].classList.contains('enemy') || laserPosition < width) {
+      console.log('moveLaserTimer Init')
+      if (cells[laserPosition].classList.contains('enemy')) {
+        clearInterval(laserTimerId)
+        console.log('collision')
         cells[laserPosition].classList.remove('enemy')
+        enemyPositions = enemyPositions.filter(enemy => enemy !== laserPosition)
+        console.log('new enemyPosition', enemyPositions)
         removeLaser()
         clearInterval(laserTimerId)
-      } else {
+      } else if (laserPosition < width) {
+        console.log('end of map')
+        removeLaser()
+        clearInterval(laserTimerId)
+      } else if (laserMovingUp) {
         removeLaser()
         laserPosition = laserPosition - width
         createLaser()
       }
-
-      if (enemiesRow1Array.includes(laserPosition)) {
-        enemiesRow1Array = enemiesRow1Array.filter(enemy => {
-          enemy !== laserPosition
-          console.log(laserPosition)
-        })
-      }
-
     }, 50)
   }
-
-
   //* DECLARE FUNCTIONS ON LOAD
-
   makeGrid()
   createAllEnemies()
   createPlayer()
-
-
   // PRINT PRESSED KEYCODE IN CONSOLE:
   // function printKey(e) {
   //   console.log(e.keyCode)
   // }
   // document.addEventListener('keyup', printKey)
 }
-
 document.addEventListener('DOMContentLoaded', init)
